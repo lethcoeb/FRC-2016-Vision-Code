892d816d8418fa40824420ec22e6ee12a0cf5e3a
Becker Lethcoe <beckerlethcoe@gmail.com>
2016-03-21 00:07:37 -0500

    push for dilly woolly

--- src/TT/ServerThread.java
+++ src/TT/ServerThread.java
@@ -7,41 +7,15 @@ import java.net.InetAddress;
 
 public class ServerThread extends Thread {
 	
-	DatagramSocket socket;
-	String msg;
-	byte[] msgByte;
-	InetAddress host;
-	int serverSocket;
-	DatagramPacket request;
-	
-	public ServerThread(){
-		init();
-	}
-	
 	public void init(){
 		
-		try{
-			socket = new DatagramSocket();
-			host = InetAddress.getByName("localhost");
-			//can be between 5800 and 5810
-			serverSocket = 5803;
-		}catch(Exception e){
-			System.out.println("Error: + " + e);
-		}
+		
 		
 	}
 	
 	public void sendInfo(boolean targetFound, double angle, double distance){
 		
-		msg = String.valueOf(targetFound) + ";" + String.valueOf(angle) + ";" + String.valueOf(distance);
-		msgByte = msg.getBytes();
 		
-		try{
-			request = new DatagramPacket(msgByte, msgByte.length, host, serverSocket);
-			socket.send(request);
-		}catch(Exception e){
-			System.out.println("Error: " + e);
-		}
 		
 	}
 	
