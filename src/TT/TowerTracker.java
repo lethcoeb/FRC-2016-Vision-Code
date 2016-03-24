b72cf4df94334f1e9f6f60af032b11cb53335015
Becker Lethcoe <beckerlethcoe@gmail.com>
2016-03-24 08:18:46 -0500

    please work
    pls

--- src/TT/TowerTracker.java
+++ src/TT/TowerTracker.java
@@ -39,6 +39,9 @@ import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.videoio.VideoCapture;
 
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
+import sun.nio.ch.Net;
+
 /**
  * 
  * @author Elijah Kaufman
@@ -54,22 +57,7 @@ public class TowerTracker {
 	// initialize opencv
 	static {
 
-		/*
-		 * try { System.load("/home/ubuntu/TowerTracker/libntcore"); } catch
-		 * (UnsatisfiedLinkError e) { System.err.println(
-		 * "Native code library failed to load.\n" + e); }
-		 * 
-		 * try { System.load("/home/ubuntu/TowerTracker/libntcore.so"); } catch
-		 * (UnsatisfiedLinkError e) { System.err.println(
-		 * "Native code library failed to load.\n" + e); }
-		 */
-
-		// System.loadLibrary("opencv_java310");
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-
-		// LibLoader.load();
-		// System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-
 		// NetworkTable.setClientMode();
 		// NetworkTable.setIPAddress("roborio-1806.local");
 	}
@@ -95,7 +83,7 @@ public class TowerTracker {
 	// the height to the top of the target in first stronghold is 97 inches
 	public static final int TOP_TARGET_HEIGHT = 97;
 	// the physical height of the camera lens
-	public static final int TOP_CAMERA_HEIGHT = 32;
+	public static final int TOP_CAMERA_HEIGHT = 40;
 
 	// camera details, can usually be found on the datasheets of the camera
 	public static final double VERTICAL_FOV = 51;
@@ -127,7 +115,10 @@ public class TowerTracker {
 	public static Rect rec;
 	public static Point center;
 	public static Point centerw;
-
+	
+	static NetworkTable table;
+	public static int goalFoundInt = 0;
+	
 	/**
 	 * 
 	 * @param args
@@ -150,27 +141,10 @@ public class TowerTracker {
 		// setup camera
 		try {
 
-			System.out.println("start try catch");
+			System.out.println("TowerTracker loading...");
+			System.out.println("Connecting to camera...");
 
 			// opens up the camera stream and tries to load it
-<<<<<<< HEAD
-			videoCapture = new VideoCapture(0);
-
-			System.out.println("video capture has been init");
-
-			// Sets exposure to a lower value
-			Runtime.getRuntime().exec("v4l2-ctl --set-ctrl exposure_auto=1");
-			Runtime.getRuntime().exec("v4l2-ctl --set-ctrl exposure_absolute=10");
-
-			// replaces the ##.## with your team number
-			// videoCapture.open("http://10.8.6.14/mjpg/video.mjpg");
-			// videoCapture.open(0);
-			System.out.println("video stream has been opened");
-
-			// Example
-			// cap.open("http://10.30.19.11/mjpg/video.mjpg");
-			// wait until it is opened
-=======
 			videoCapture = new VideoCapture();
 			//this connects to an onboard cam
 			//http://roborio-1806-frc.local:5800/?action=stream
@@ -181,33 +155,30 @@ public class TowerTracker {
 			System.out.println("Video capture has been initialized...");
 			
 			//Wait until videocapture is opened
->>>>>>> fork-github
 			while (!videoCapture.isOpened()) {
 				
 					videoCapture.open("http://roborio-1806-frc.local:5800/?action=stream");
 				
 			}
+			System.out.println("Video stream has been opened...");
 
 			connectedToCamera = true;
+			
+			
+			//NetworkTables sterf
+			NetworkTable.setClientMode();
+			NetworkTable.setTeam(1806);
+			NetworkTable.initialize();
+			table = NetworkTable.getTable("TowerTracker");
+			//wait for networktables to connect
+			while(!table.isConnected()){
+				
+			}
+			
 
 		} catch (Exception e) {
 			System.out.println("Failed to connect to camera: " + e);
 			connectedToCamera = false;
-		}
-
-		DatagramSocket socket = null;
-		InetAddress roborio = null;
-		try {
-			roborio = InetAddress.getByName("roboRIO-1806-FRC.local");
-			connectedToRio = true;
-		} catch (UnknownHostException e) {
-			System.out.println("Failed to connect to RoboRIO: " + e);
-			connectedToRio = false;
-		}
-		try {
-			socket = new DatagramSocket(5800);
-		} catch (SocketException ex) {
-			System.out.println(ex);
 		}
 
 		// main running loop
@@ -215,9 +186,6 @@ public class TowerTracker {
 		while (shouldRun) {
 			try {
 
-<<<<<<< HEAD
-				if (connectedToCamera) {
-=======
 				if (!videoCapture.isOpened()) {
 					System.out.println("Lost camera connection");
 					connectedToCamera = false;
@@ -236,54 +204,38 @@ public class TowerTracker {
 						//wait
 					}
 				}
->>>>>>> fork-github
 
-					if (!videoCapture.isOpened()) {
-						System.out.println("Lost camera connection");
-						connectedToCamera = false;
-					} else {
-						connectedToCamera = true;
-					}
-
-					if (calibrationMode) {
-						loops++;
-						if (loops >= 50) {
-							loops = 0;
-							setParameters();
-							Imgcodecs.imwrite("/home/ubuntu/TowerTracker/output.png", matOriginal);
-						}
-					}
-					
-					// time to actually process the acquired images
-					processImage();
-					fps = (1000 / (System.currentTimeMillis() + 1 - lastTime));
-					System.out.println("Processing FPS: " + (fps + lastfps) / 2);
-					lastTime = System.currentTimeMillis();
-					lastfps = fps;
-
-					if (connectedToCamera) {
-						packet = generateDatagramPacket(goalFound, distance, azimuth, roborio);
-					} else {
-						System.out.println("Lost connection to cam");
-						packet = generateDatagramPacket(false, 0.0, 0.0, roborio);
-
-					}
-					socket.send(packet);
-
-					// st.sendInfo(goalFound, azimuth, distance);
-				} else {
-					// begin to try and reconnect
-					try {
-						roborio = InetAddress.getByName("roboRIO-1806-FRC.local");
-						connectedToRio = true;
-					} catch (UnknownHostException e) {
-						System.out.println("Failed to connect to RoboRIO: " + e);
-						connectedToRio = false;
+				if (calibrationMode) {
+					loops++;
+					if (loops >= 50) {
+						loops = 0;
+						setParameters();
+						// Imgcodecs.imwrite("/home/ubuntu/TowerTracker/output.png",
+						// matOriginal);
+						Imgcodecs.imwrite("C:/Users/Becker/Desktop/TowerTracker/output.png", matOriginal);
 					}
 				}
+
+				// time to actually process the acquired images
+				processImage();
+				if(goalFound){
+					goalFoundInt = 1;
+				}else{
+					goalFoundInt = 0;
+				}
+				table.putNumber("GoalFound", goalFoundInt);
+				table.putNumber("AngleToGoal", azimuth);
+				table.putNumber("Distance", distance);
+				fps = (1000 / (System.currentTimeMillis() + 1 - lastTime));
+				System.out.println("Processing FPS: " + (fps + lastfps) / 2);
+				//System.out.println(azimuth);
+				lastTime = System.currentTimeMillis();
+				lastfps = fps;
+
+				// st.sendInfo(goalFound, azimuth, distance);
+
 			} catch (Exception e) {
 				e.printStackTrace();
-				break;
 			}
 		}
 
@@ -336,9 +288,9 @@ public class TowerTracker {
 			distance = (TOP_TARGET_HEIGHT - TOP_CAMERA_HEIGHT)
 					/ Math.tan((y * VERTICAL_FOV / 2.0 + CAMERA_ANGLE) * Math.PI / 180);
 			// angle to target...would not rely on this
-			targetX = rec.tl().x + rec.width / 2;
-			targetX = (2 * (targetX / matOriginal.width())) - 1;
-			azimuth = normalize360(targetX * HORIZONTAL_FOV / 2.0 + 0);
+			targetX = rec.tl().x + rec.width / 2.000;
+			targetX = (2 * (targetX / matOriginal.width())) - 1.000;
+			azimuth = normalize360(targetX * HORIZONTAL_FOV / 2.000 + 0.000);
 			// drawing info on target
 			center = new Point(rec.br().x - rec.width / 2 - 15, rec.br().y - rec.height / 2);
 			centerw = new Point(rec.br().x - rec.width / 2 - 15, rec.br().y - rec.height / 2 - 20);
@@ -360,37 +312,24 @@ public class TowerTracker {
 	 */
 	public static double normalize360(double angle) {
 		while (angle >= 360.0) {
-			angle -= 360.0;
+			angle -= 360.000;
 		}
 		while (angle < 0.0) {
-			angle += 360.0;
+			angle += 360.000;
 		}
 		return angle;
-	}
-
-	public static DatagramPacket generateDatagramPacket(Boolean goal, Double dist, Double angle, InetAddress roborio) {
-		buf = ByteBuffer.allocate(20);
-		buf.putInt(goal ? (int) 1 : (int) 0);
-		buf.putDouble(dist);
-		buf.putDouble(angle);
-		/*
-		 * //code for reading back from the buffer buf.position(0);
-		 * System.out.println(buf.getInt());
-		 * System.out.println(buf.getDouble());
-		 * System.out.println(buf.getDouble());
-		 */
-		return new DatagramPacket(buf.array(), buf.capacity(), roborio, 5800);
-
 	}
 
 	public static void setParameters() {
 		int iterations = 0;
 		try {
-			List<String> lines = Files.readAllLines(Paths.get("/home/ubuntu/TowerTracker/parameters.txt"),
+			// List<String> lines =
+			// Files.readAllLines(Paths.get("/home/ubuntu/TowerTracker/parameters.txt"),
+			// StandardCharsets.UTF_8);
+			List<String> lines = Files.readAllLines(Paths.get("C:/Users/Becker/Desktop/TowerTracker/parameters.txt"),
 					StandardCharsets.UTF_8);
 			Iterator<String> iterator = lines.iterator();
 			while (iterator.hasNext()) {
-				// TO TEST!
 				String str = iterator.next();
 				if (str.contains("#")) {
 					iterator.remove();
@@ -438,7 +377,7 @@ public class TowerTracker {
 			double[] upper = new double[] { (double) maxHue, (double) maxSaturation, (double) maxValue };
 			UPPER_BOUNDS.set(upper);
 
-			System.out.println("maxValue: " + maxValue);
+			System.out.println("Parameters set.");
 
 		} catch (Exception e) {
 			e.printStackTrace();
